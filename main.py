c6ac03267edfbf295f4dd9287158fb980b73af4a
Matt Andreko <mandreko@gmail.com>
2025-05-02 21:52:19 -0400

    Minor fixes

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
+"""Script providing a method to update date-added in Plex library."""
+
 import argparse
-import os
 import sys
 
 from plexapi.myplex import MyPlexAccount
 from plexapi.server import PlexServer
 
-# Example:
-# python main.py -a $PLEX_TOKEN -H "https://12-34-56-78.123456789.plex.direct:32400" -l Movies -t "Comedy Central Roast of Pamela Anderson"
 parser = argparse.ArgumentParser()
 parser.add_argument("-H", "--host",required=True)
 parser.add_argument("-l", "--library",required=True)
@@ -20,13 +19,13 @@ parser.add_argument("-d", "--date-added", default="2020-01-01 00:00:00")
 args = parser.parse_args()
 
 if args.auth_token is not None:
-	plex = PlexServer(args.host, args.auth_token)
+    plex = PlexServer(args.host, args.auth_token)
 elif args.username is not None and args.password is not None:
-	account = MyPlexAccount(args.username, args.password)
-	plex = account.resource(args.host).connect()
+    account = MyPlexAccount(args.username, args.password)
+    plex = account.resource(args.host).connect()
 else:
-	print("You must specify a user/pass or auth token")
-	exit(1)
+    print("You must specify a user/pass or auth token")
+    sys.exit(1)
 
 library = plex.library.section("Movies")
 video = library.get(title="Comedy Central Roast of Pamela Anderson")
